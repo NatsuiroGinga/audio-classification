ec03faf0a32a3b30e422128fd4c2d4c3c3eddf2a
NatsuiroGinga <duyueyeweiliang@gmail.com>
2025-09-23 16:22:38 +0800

    更新测试脚本，修改说话人文件和测试列表路径为 scene-2 数据集

--- scripts/test.sh
+++ scripts/test.sh
@@ -17,8 +17,8 @@ fi
 
 python3 ./benchmark_pipeline.py \
   --silero-vad-model ../models/vad/silero_vad.onnx \
-  --speaker-file ../dataset/train-speaker.txt \
-  --test-list ../dataset/test-speaker.txt \
+  --speaker-file ../dataset/scene-2-speaker.txt \
+  --test-list ../dataset/scene-2-speaker.txt \
   --model ../models/speaker-recongition/3dspeaker_speech_eres2net_base_sv_zh-cn_3dspeaker_16k.onnx \
   --sense-voice ../models/asr/sherpa-onnx-sense-voice-zh-en-ja-ko-yue-2024-07-17/model.int8.onnx \
   --provider cuda \
@@ -29,7 +29,6 @@ python3 ./benchmark_pipeline.py \
   --ref-text-list ../dataset/transcription/scene-2_transcription \
   --emb-cache-dir ../cache \
   --cpu-normalize \
-  --load-speaker-embeds ../cache/speakers.npz \
   --plot-cpu \
   # 初次运行时, 注释掉--load-speaker-embeds, 启用--save-speaker-embeds, 先生成说话人embeddings缓存
   # 后续使用相同数据集时, 注释掉--save-speaker-embeds, 启用--load-speaker-embeds, 直接加载缓存
